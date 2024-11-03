d8172ce77425dadbd4e3862677d2ecb3171e9be6
Pablofl01 <pablofl01@gmail.com>
2024-11-03 13:39:34 +0100

    Code cleanup

--- src/controllers/proposalController.js
+++ src/controllers/proposalController.js
@@ -8,16 +8,6 @@ const User = require('../models/user');
 
 const helpers = require('../helpers');
 
-/*const createProposal = async (req, res) => {
-    try {
-        const newProposal = new Proposal(req.body);
-        await newProposal.save();
-        res.status(201).json(newProposal);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}*/
-
 const getProposals = async (req, res, next) => {
     try {
         const categories = await helpers.retrieveCategories();
@@ -82,33 +72,6 @@ const getProposal = async (req, res, next) => {
         return next(new InternalServerError("Ha ocurrido un error al obtener la propuesta."));
     }
 }
-
-/*const deleteProposal = async (req, res) => {
-    try {
-        await proposal.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: 'Proposal deleted' });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}*/
-
-/*const getProposalByCategory = async (req, res) => {
-    try {
-        const proposals = await proposal.find({ categories: req.params.category });
-        res.status(200).json(proposals);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-} */
-
-/*const getProposalsCategories = async (req, res) => {
-    try {
-        const categories = await proposal.distinct('categories');
-        res.status(200).json(categories);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}*/
 
 const addSupporter = async (req, res, next) => {
     try {
@@ -250,54 +213,11 @@ const sendProposalAsDraft = async (req, res, next) => {
     }
 }
 
-/*const getDraftProposals = async (req, res) => {
-    try {
-        const proposals = await proposal.find({ isDraft: true });
-        res.status(200).json(proposals);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}*/
-
-/*const deleteDraftProposal = async (req, res) => {
-    try {
-        await proposal.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: 'Draft proposal deleted' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}*/
-
-/*const approveDraftProposal = async (req, res) => {
-    try {
-        let userProposing = new Set();
-        let deleteProposals = req.body.deleteProposals;
-        for (let proposalId of deleteProposals) {
-            const proposal = await proposal.findById(proposalId);
-            userProposing.add(proposal.usersDrafting);
-            await proposal.findByIdAndDelete(proposalId);
-        }
-        const newProposal = new proposal(req.body);
-        newProposal.isDraft = false;
-        newProposal.usersDrafting = userProposing;
-        await newProposal.save();
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}*/
-
 module.exports = {
-    //createProposal,
     getProposals,
     getProposal,
-    //deleteProposal,
-    //getProposalByCategory,
-    //getProposalsCategories,
     addSupporter,
     removeSupporter,
     getDraftForm,
     sendProposalAsDraft,
-    //getDraftProposals,
-    //deleteDraftProposal,
-    //approveDraftProposal
 }
--- src/routes/proposal.js
+++ src/routes/proposal.js
@@ -5,8 +5,6 @@ const middlewares = require('../middlewares');
 
 router.get('/', proposalController.getProposals);
 router.get('/:id([a-f,0-9]{24})', proposalController.getProposal);
-//router.get('/categories', proposalController.getProposalsCategories);
-//router.get('/category/:category', proposalController.getProposalByCategory);
 
 router.use(middlewares.checkLogin);
 
@@ -14,14 +12,5 @@ router.get('/draft-form', proposalController.getDraftForm);
 router.post('/draft', proposalController.sendProposalAsDraft);
 router.post('/:id([a-f,0-9]{24})/support', proposalController.addSupporter);
 router.delete('/:id([a-f,0-9]{24})/support', proposalController.removeSupporter);
-
-router.use(middlewares.restrictAdmins);
-
-//router.post('/draft/:id([a-f,0-9]{24})/accept', proposalController.approveDraftProposal);
-//router.delete('/draft/:id([a-f,0-9]{24})', proposalController.deleteDraftProposal);
-//router.get('/draft', proposalController.getDraftProposals);
-
-//router.post('/', proposalController.createProposal);
-//router.delete('/:id([a-f,0-9]{24})', proposalController.deleteProposal);
 
 module.exports = router;
