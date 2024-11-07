24ab56e4327ed619307d9de36b3887a9a3fa25a9
Pablofl01 <pablofl01@gmail.com>
2024-11-07 20:50:30 +0100

    Implemented stats admin page

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -55,10 +55,49 @@ const getAdmin = async (req, res, next) => {
         res.status(404).json({ message: error.message });
     }}
 
+// Función para obtener las estadísticas: número total de usuarios, número total de propuestas, número de propuestas borrador, número de propuestas publicadas, propuesta con más apoyos, número máximo de apoyos, número medio de apoyos, número de propuestas por centro, número de propuestas por afiliación.
+
+const getStats = async (req, res, next) => {
+    try {
+        const totalUsers = await User.countDocuments();
+        const totalProposals = await Proposal.countDocuments();
+        const draftProposals = await Proposal.countDocuments({ isDraft: true });
+        const publishedProposals = await Proposal.countDocuments({ isDraft: false });
+
+        const proposals = await Proposal.find();
+        const proposalWithMostSupport = proposals.reduce((max, proposal) => proposal.supporters > max.supporters ? proposal : max, proposals[0]);
+        const maxSupport = proposalWithMostSupport ? proposalWithMostSupport.supporters : 0;
+        const averageSupport = proposals.length ? proposals.reduce((sum, proposal) => sum + proposal.supporters, 0) / proposals.length : 0;
+
+        const proposalsByCentre = await Proposal.aggregate([
+            { $group: { _id: "$centre", count: { $sum: 1 } } }
+        ]);
+
+        const proposalsByAffiliation = await Proposal.aggregate([
+            { $group: { _id: "$affiliation", count: { $sum: 1 } } }
+        ]);
+
+        res.status(200).render('stats', {
+            totalUsers,
+            totalProposals,
+            draftProposals,
+            publishedProposals,
+            proposalWithMostSupport,
+            maxSupport,
+            averageSupport,
+            proposalsByCentre,
+            proposalsByAffiliation
+        });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 module.exports = {
     getIndex,
     getProcess,
     getDates,
     getCandidates,
-    getAdmin
+    getAdmin,
+    getStats
 }
